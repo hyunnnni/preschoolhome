da8f5d0b543dbfb708f3eb30d1a8169df676dcec
sunxno <sunxno@naver.com>
2024-01-11 17:53:27 +0900

    01-11

--- src/main/java/com/preschool/preschoolhome/common/AppProperties.java
+++ src/main/java/com/preschool/preschoolhome/common/AppProperties.java
@@ -3,10 +3,13 @@ package com.preschool.preschoolhome.common;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 @Getter
+@Configuration
 @ConfigurationProperties(prefix = "app")
 public class AppProperties {
+
 
     private final Jwt jwt = new Jwt();
 
