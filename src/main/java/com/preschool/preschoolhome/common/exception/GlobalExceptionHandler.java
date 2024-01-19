53fa32ef62a6a665cbbf0d121fe38e5b430b02b2
hyun <kimkh8056k@gmail.com>
2024-01-19 15:30:07 +0900

    24/01/19

--- src/main/java/com/preschool/preschoolhome/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/preschool/preschoolhome/common/exception/GlobalExceptionHandler.java
@@ -2,9 +2,13 @@ package com.preschool.preschoolhome.common.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Slf4j
@@ -27,6 +31,20 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<Object> handleRestApiException(RestApiException e){
         log.warn("handleRestApiException",e);
         return handleExceptionInternal(e.getErrorCode());
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)//Valid를 사용한 부분에서 정해놓은 거 외에 값이 들어와 에러가 발생했을 때의 메소드
+    public ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
+        log.warn("handleMethodArgumentNotValidException", e);
+
+        List<String> errors = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(lfe -> lfe.getDefaultMessage())
+                .collect(Collectors.toList());
+
+
+        String errStr = "["+String.join( ", " , errors)+"]";
+        return handleExceptionInternal(CommonErrorCode.INVALID_PARAMETER, errors.toString());
     }
 
     private ResponseEntity<Object> handleExceptionInternal(ErrorCode errorCode){
