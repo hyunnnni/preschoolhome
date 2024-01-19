d577318a0ee5de8ba63d02de26551df0e784573b
hyun <kimkh8056k@gmail.com>
2024-01-19 15:41:05 +0900

    24/01/19 경현

---
+++ src/main/java/com/preschool/preschoolhome/notice/model/SelAllNoticeDto.java
@@ -0,0 +1,43 @@
+package com.preschool.preschoolhome.notice.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.preschool.preschoolhome.common.utils.Const;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Min;
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+@Data
+@Schema(title = "알림장 조회 접근 유저에 따라 다르게 조회에 필요한 데이터")
+public class SelAllNoticeDto {
+    @Schema(title = "페이징 시 필요한 데이터")
+    private int page;
+
+    @Schema(title = "이 페이지에 접근하는 유저의 등급 PK")
+    @Range(min = 1, max = 3, message = "해당 페이지에 접근할 권한이 없습니다.")
+    private int ilevel;
+
+    @Schema(title = "학부모 유저가 접근 시 본인과 연결된 원아 PK / 연결된 모든 원아 조회 시 = 0")
+    private int ikid;
+
+    @Schema(title = "선생님 유저가 접근 시 원하는 원아들 반 PK /전체 조회 시 = 0")
+    @Range(min = 0, max = 3, message = "해당 반으로 검색되는 아이가 없습니다.")
+    private int iclass;
+
+    @Schema(title = "해당 원아의 알림장 업로드 년도/ 전체 조회 시 = null")
+    private String year;
+
+    @Schema(title = "이 페이지에 접근한 유저(선생님 OR 학부모)의 PK")
+    @Min(value = 1, message = "로그인한 유저를 알 수 없습니다")
+    private int loginedIuser;
+
+    @JsonIgnore
+    private int startIdx;
+    @JsonIgnore
+    private int rowCount;
+
+    public void setPage(int page) {
+        this.rowCount = Const.PAGE_ROWCOUNT;
+        this.startIdx = (page-1)*rowCount;
+    }
+}
---
+++ src/main/java/com/preschool/preschoolhome/notice/model/SelAllNoticeVo.java
@@ -0,0 +1,25 @@
+package com.preschool.preschoolhome.notice.model;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+@Data
+@Schema(title = "알림장 조회 응답데이터")
+public class SelAllNoticeVo {
+    @Schema(title = "알림장 PK")
+    private int inotice;
+    @Schema(title = "알림장 제목")
+    private String noticeTitle;
+    @Schema(title = "알림장 내용")
+    private String noticeContents;
+    @Schema(title = "알림장을 받는 아이 이름")
+    private String kidNm;
+    @Schema(title = "알림장을 받는 아이 반")
+    private int iclass;
+    @Schema(title = "해당 알림장에 사진 포함 1: 유/0 : 무")
+    private int picCheck;
+    @Schema(title = "알림장 업로드일")
+    private String createdAt;
+    @Schema(title = "실패 응답값")
+    private int result;
+}
