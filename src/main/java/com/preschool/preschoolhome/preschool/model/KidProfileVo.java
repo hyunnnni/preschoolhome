5d7097e766f77a2d71b15920afbae3151115e52a
sunxno <sunxno@naver.com>
2024-01-19 17:05:44 +0900

    19

--- src/main/java/com/preschool/preschoolhome/preschool/model/KidProfileVo.java
+++ src/main/java/com/preschool/preschoolhome/preschool/model/KidProfileVo.java
@@ -1,10 +1,14 @@
 package com.preschool.preschoolhome.preschool.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class KidProfileVo {
+    @Schema(title = "반 PK")
     private int iclass;
+    @Schema(title = "아이 이름")
     private String kidNm;
+    @Schema(title = "아이 사진")
     private String profile;
 }
--- src/main/java/com/preschool/preschoolhome/preschool/model/TeacherProfileVo.java
+++ src/main/java/com/preschool/preschoolhome/preschool/model/TeacherProfileVo.java
@@ -1,11 +1,16 @@
 package com.preschool.preschoolhome.preschool.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class TeacherProfileVo {
+    @Schema(title = "반 PK")
     private int iclass;
+    @Schema(title = "선생님 이름")
     private String teacherNm;
+    @Schema(title = "선생님 사진")
     private String teacherProfile;
+    @Schema(title = "선생님 소개")
     private String teacherIntroduce;
 }
