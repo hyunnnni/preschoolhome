2e7a0baadbeedb61a953d2c06382829b439fdb5b
GYEONGROK11 <gpol123@naver.com>
2024-01-22 16:17:04 +0900

    2024/01/22 경록

--- src/main/java/com/preschool/preschoolhome/parent/model/ParentInsDto.java
+++ src/main/java/com/preschool/preschoolhome/parent/model/ParentInsDto.java
@@ -11,20 +11,21 @@ public class ParentInsDto {
     private int ikid;
     @JsonIgnore
     private int iparent;
-    @Schema(title = "이메일")
-    private String prEmail;
+
     @Schema(title = "아이와의 관계")
     private int irelation;
     @Schema(title = "아이디 중복체크")
     private int isValid; // 중복체크가 되었는지 확인 (1: 문제없음, 나머지: 문제있음)
     @Schema(title = "부모이름")
     private String parentNm;
-    @Schema(title = "휴대폰번호")
-    private String phoneNb;
     @Schema(title = "아이디")
     private String uid;
     @Schema(title = "비밀번호")
     private String upw;
+    @Schema(title = "휴대폰번호")
+    private String phoneNb;
+    @Schema(title = "이메일")
+    private String prEmail;
     @JsonIgnore
     private String code;
 
