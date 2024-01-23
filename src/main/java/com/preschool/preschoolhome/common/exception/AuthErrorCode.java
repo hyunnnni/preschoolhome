74dbdfa55e19f1b1f1e093e610ba09ae3241a598
sunxno <sunxno@naver.com>
2024-01-23 15:09:07 +0900

    fullnotice

--- src/main/java/com/preschool/preschoolhome/common/exception/AuthErrorCode.java
+++ src/main/java/com/preschool/preschoolhome/common/exception/AuthErrorCode.java
@@ -13,7 +13,6 @@ public enum AuthErrorCode implements ErrorCode {
     CHECK_CODE(HttpStatus.BAD_REQUEST,"식별코드를 확인하세요."),
     NOT_ENTER_ID(HttpStatus.BAD_REQUEST,"아이디를 입력하세요."),
     ALREADY_EXIST_ID(HttpStatus.BAD_REQUEST,"이미 존재하는 아이디"),
-    SUCCES_SIGNUP(HttpStatus.OK,"회원가입완료"),
     NOT_EXIST_USER_ID(HttpStatus.NOT_FOUND,"아이디를 확인해주세요"),
     VALID_PASSWORD(HttpStatus.NOT_FOUND,"비밀번호를 확인해주세요"),
     CHANGE_JUST_ONETHING(HttpStatus.BAD_REQUEST, "한개 이상 입력하세요"),
