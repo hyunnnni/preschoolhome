4ec805ba5c7b4b0a77971dacab02c49d9a1107d3
GYEONGROK11 <gpol123@naver.com>
2024-01-24 12:42:36 +0900

    2024/01/23 경록

--- src/main/java/com/preschool/preschoolhome/teacher/model/TeacherEntity.java
+++ src/main/java/com/preschool/preschoolhome/teacher/model/TeacherEntity.java
@@ -1,5 +1,6 @@
 package com.preschool.preschoolhome.teacher.model;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -10,6 +11,7 @@ public class TeacherEntity {
     private String teacherUid;
     private String teacherUpw;
     private String teacherNm;
+    @Pattern(regexp = "^[a-z|A-Z|0-9|_|-]+@([a-z|0-9]{3,}\\.[a-z]{2,}|[a-z|0-9]{3,}\\.[a-z]{2,}\\.[a-z]{2,})$")
     private String tcEmail;
     private String teacherIntroduce;
     private String teacherProfile;
