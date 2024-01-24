432ae3eb47b32b391047dd4f42c2c539083afbf3
sunxno <sunxno@naver.com>
2024-01-24 15:48:25 +0900

    fullnotice

--- src/main/java/com/preschool/preschoolhome/fullnotice/model/SelFullNoticeFixVo.java
+++ src/main/java/com/preschool/preschoolhome/fullnotice/model/SelFullNoticeFixVo.java
@@ -6,5 +6,6 @@ import lombok.Data;
 @Data
 @Schema(title = "공지사항")
 public class SelFullNoticeFixVo {
+    @Schema(title = "유치원 소식 공지")
     private int fullNoticeFix;
 }
--- src/main/java/com/preschool/preschoolhome/fullnotice/model/SelNoticeVo.java
+++ src/main/java/com/preschool/preschoolhome/fullnotice/model/SelNoticeVo.java
@@ -8,9 +8,14 @@ import java.util.List;
 @Data
 @Schema(title = "유치원 소식 상세 조회")
 public class SelNoticeVo {
+    @Schema(title = "유치원 소식 제목")
     private String fullTitle;
+    @Schema(title = "유치원 소식 내용")
     private String fullContents;
+    @Schema(title = "유치원 소식 작성자")
     private String writer;
+    @Schema(title = "유치원 소식 등록일")
     private String createdAt;
+    @Schema(title = "유치원 소식 사진")
     private List<String> pics;
 }
