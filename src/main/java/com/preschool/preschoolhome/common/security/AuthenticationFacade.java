9122b65ee0675b799a1d24f0e8137de5491e05b0
GYEONGROK11 <gpol123@naver.com>
2024-01-25 14:35:48 +0900

    2024/01/23 경록

--- src/main/java/com/preschool/preschoolhome/common/security/AuthenticationFacade.java
+++ src/main/java/com/preschool/preschoolhome/common/security/AuthenticationFacade.java
@@ -9,9 +9,12 @@ import org.springframework.stereotype.Component;
 public class AuthenticationFacade {
     public MyUserDetails getLoginUser(){
 
-        return (MyUserDetails)SecurityContextHolder
+        return SecurityContextHolder
+                .getContext() == null
+                ? null
+                : (MyUserDetails)SecurityContextHolder
                 .getContext()
-                .getAuthentication() //토큰값이 넘어옴
+                .getAuthentication() //토큰값이 넘어옴  //null이면 로그인 x, null이 아니면 로그인 상태
                 .getPrincipal(); //오브젝트에서 형변환
     }
 
