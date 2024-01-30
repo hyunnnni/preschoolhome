1625f697d1e8c220252ca1d0cecef8d23284be9f
GYEONGROK11 <gpol123@naver.com>
2024-01-30 15:35:21 +0900

    2024/01/30 경록

--- src/main/java/com/preschool/preschoolhome/fullnotice/model/AllFullNoticeSelVo.java
+++ src/main/java/com/preschool/preschoolhome/fullnotice/model/AllFullNoticeSelVo.java
@@ -1,12 +1,16 @@
 package com.preschool.preschoolhome.fullnotice.model;
 
 import com.preschool.preschoolhome.album.model.AlbumSelVo;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@Schema(title = "소식 조회")
 public class AllFullNoticeSelVo {
+    @Schema(title = "소식 개수")
     private int noticeCnt;
+    @Schema(title = "소식 리스트")
     private List<SelFullNoticeVo> list;
 }
