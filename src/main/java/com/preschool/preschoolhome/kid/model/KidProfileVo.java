8046f2d11a0a126e6ec9b78827f19c465fe9fa7d
GYEONGROK11 <gpol123@naver.com>
2024-01-30 17:12:49 +0900

    2024/01/30 경록

--- src/main/java/com/preschool/preschoolhome/kid/model/KidProfileVo.java
+++ src/main/java/com/preschool/preschoolhome/kid/model/KidProfileVo.java
@@ -18,17 +18,17 @@ public class KidProfileVo {
     private int iclass;
     @Schema(title = "성별")
     private int gender;
-    @Schema(title = "프로필", type = "String")
+    @Schema(title = "프로필")
     private String profile;
-    @Schema(title = "출생일", type = "String")
+    @Schema(title = "출생일")
     private String birth;
-    @Schema(title = "주소", type = "String")
+    @Schema(title = "주소")
     private String address;
-    @Schema(title = "발달사항", type = "List")
+    @Schema(title = "발달사항")
     List<KidGrowth> growths = new ArrayList<>();
-    @Schema(title = "부모님", type = "List")
+    @Schema(title = "부모님")
     List<KidParent> parents = new ArrayList<>();
-    @JsonIgnore
+    @Schema(title = "식별코드")
     private String code;
     @Schema(title = "메모", type = "String")
     private String memo;
