000c302ad2e32b107766d8c008b20ff5193bc164
hyun <kimkh8056k@gmail.com>
2024-01-30 20:34:50 +0900

    24/01/30 경현

--- src/test/java/com/preschool/preschoolhome/kid/KidControllerTest.java
+++ src/test/java/com/preschool/preschoolhome/kid/KidControllerTest.java
@@ -2,26 +2,19 @@ package com.preschool.preschoolhome.kid;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.preschool.preschoolhome.MockMvcConfig;
-import com.preschool.preschoolhome.common.utils.ResVo;
 import com.preschool.preschoolhome.kid.model.KidProfileVo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.PathVariable;
 
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
