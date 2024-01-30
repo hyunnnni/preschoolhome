d1b239c04516c8db6b68e729d2286cede463dab7
GYEONGROK11 <gpol123@naver.com>
2024-01-30 15:17:02 +0900

    2024/01/30 경록

--- src/main/java/com/preschool/preschoolhome/parent/model/ParentInsDto.java
+++ src/main/java/com/preschool/preschoolhome/parent/model/ParentInsDto.java
@@ -17,6 +17,7 @@ public class ParentInsDto {
     @Schema(title = "아이디 중복체크")
     private int isValid; // 중복체크가 되었는지 확인 (1: 문제없음, 나머지: 문제있음)
     @Schema(title = "부모이름")
+
     private String parentNm;
     @Schema(title = "아이디")
     private String uid;
