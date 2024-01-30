21a2386ce52a29ee61ac101b8b69849992710323
sunxno <sunxno@naver.com>
2024-01-30 19:54:27 +0900

    24/01/30

--- src/test/java/com/preschool/preschoolhome/kid/KidControllerTest.java
+++ src/test/java/com/preschool/preschoolhome/kid/KidControllerTest.java
@@ -1,13 +1,66 @@
 package com.preschool.preschoolhome.kid;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.preschool.preschoolhome.MockMvcConfig;
+import com.preschool.preschoolhome.common.utils.ResVo;
+import com.preschool.preschoolhome.kid.model.KidProfileVo;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@MockMvcConfig
+@WebMvcTest(KidController.class)
 class KidControllerTest {
 
+    @Autowired
+    private MockMvc mvc;
+    @MockBean
+    private KidService service;
+    @Autowired
+    private ObjectMapper mapper;
+
     @Test
-    void getKidProfile() {
+    void getKidProfile() throws Exception {
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>(); //스프링 꺼 해쉬맵과 비슷?
+        params.add("year","2024");
+        params.add("loginedIuser","1");
+
+        KidProfileVo result = new KidProfileVo();
+        result.setKidNm("최승철");
+        result.setIclass(1);
+        result.setGender(1);
+
+        given(service.kidProfile(any(), any())).willReturn(result);
+
+        mvc.perform(
+                        MockMvcRequestBuilders
+                                .get("/api/kid")
+                                .params((params))
+                )//검증하는부분
+                .andExpect(status().isOk())
+                .andExpect(content().string(mapper.writeValueAsString(result)))
+                .andDo(print());
+
+        verify(service).kidProfile(any(),any());
     }
 
     @Test
