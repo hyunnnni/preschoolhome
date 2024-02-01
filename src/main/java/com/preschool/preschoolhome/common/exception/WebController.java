3d564081bf35f31f1d8a20d449a8e80f045a0794
GYEONGROK11 <gpol123@naver.com>
2024-02-01 12:41:47 +0900

    2024/02/01 경록

--- src/main/java/com/preschool/preschoolhome/common/exception/WebController.java
+++ src/main/java/com/preschool/preschoolhome/common/exception/WebController.java
@@ -6,6 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebController implements ErrorController {
+    // 1. 이 방법
+    // 2. 404 만 캐치해서 index.html
+    // 3. /* , /*/* , /*/*/*, /*/*/*/* 이 주소로 왔을때 index.html
+    // 4. ViewResolver => FORWARD /
+
+
     @GetMapping({"/","/error"})
     public String index(){
         return "index.html";
