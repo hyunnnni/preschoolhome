8ae51b82a72ef6ee490fe448ed8ad18185231a65
GYEONGROK11 <gpol123@naver.com>
2024-02-19 17:30:08 +0900

    2024/02/19 경록

--- src/main/java/com/preschool/preschoolhome/memory/MemoryController.java
+++ src/main/java/com/preschool/preschoolhome/memory/MemoryController.java
@@ -51,7 +51,7 @@ public class MemoryController {
         return service.getFromKids();
     }
 
-    @DeleteMapping
+    @DeleteMapping("/del")
     @Operation(summary = "3차 추억 앨범 글 삭제", description = """
         추억 앨범의 사진, 댓글 모두 삭제""")
     public ResVo delmemory (int imemory) {
