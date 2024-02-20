320aba7ef7f8b7bae4b317e58433dc918f9c0095
GYEONGROK11 <gpol123@naver.com>
2024-02-20 13:02:44 +0900

    2024/02/20 경록

--- src/main/java/com/preschool/preschoolhome/memory/model/MemoryRepository.java
+++ src/main/java/com/preschool/preschoolhome/memory/model/MemoryRepository.java
@@ -1,7 +1,14 @@
 package com.preschool.preschoolhome.memory.model;
 
 import com.preschool.preschoolhome.entity.MemoryEntity;
+import com.preschool.preschoolhome.entity.TeacherEntity;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.awt.print.Pageable;
+import java.util.List;
+
 public interface MemoryRepository extends JpaRepository<MemoryEntity, Integer> {
+    @EntityGraph(attributePaths = {"teacherEntity"})
+    List<MemoryEntity> findAllByTeacherEntity(TeacherEntity teacherEntity, Pageable pageable);
 }
