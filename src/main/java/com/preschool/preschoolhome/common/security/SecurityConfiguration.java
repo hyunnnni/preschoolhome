75d4c46ad1ed4f20df9423d8b220cdf23d3c628f
GYEONGROK11 <gpol123@naver.com>
2024-02-24 11:55:28 +0900

    2024/02/24 경록

--- src/main/java/com/preschool/preschoolhome/common/security/SecurityConfiguration.java
+++ src/main/java/com/preschool/preschoolhome/common/security/SecurityConfiguration.java
@@ -44,8 +44,9 @@ public class SecurityConfiguration {
                         ,"/api/notice/tea"
                         ,"/api/memory"
                         ,"/api/teacher"
-                        //).authenticated()
-                        //).hasAnyRole("TEACHER","ADMIN")
+                        //).authenticated())
+                        //).hasAnyRole("TEACHER","ADMIN"))
+                        //.authorizeHttpRequests(auth -> auth
                         //.requestMatchers(HttpMethod.POST
                         ,"/api/teacher/signup"
                         //).authenticated()
@@ -54,8 +55,9 @@ public class SecurityConfiguration {
                         ,"/api/album/comment"
                         ,"/api/notice/par"
                         ,"/api/notice/comment"
-                        ).authenticated()
-                        //).hasAnyRole("PARENT","TEACHER","ADMIN")
+                        ).authenticated())
+                        //).hasAnyRole("PARENT","TEACHER","ADMIN"))
+                        .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.GET
                         ,"/api/album/edit"
                         ,"/api/full/edit"
@@ -68,12 +70,14 @@ public class SecurityConfiguration {
                         ,"/api/teacher/parent"
                         ,"/api/teacher/edit"
                         ,"/api/teacher/parentedit"
-                        //).authenticated()
-                        //).hasAnyRole("TEACHER","ADMIN")
+                        //).authenticated())
+                        //).hasAnyRole("TEACHER","ADMIN"))
+                        //.authorizeHttpRequests(auth -> auth
                         //.requestMatchers(HttpMethod.GET
                         ,"/api/teacher"
-                        //).authenticated()
-                        //).hasAnyRole("ADMIN")
+                        //).authenticated())
+                        //).hasAnyRole("ADMIN"))
+                        //.authorizeHttpRequests(auth -> auth
                         //.requestMatchers(HttpMethod.GET
                         ,"/api/album"
                         ,"/api/album/listall"
@@ -89,8 +93,9 @@ public class SecurityConfiguration {
                         ,"/api/parent/putparent"
                         ,"/api/parent/kidadd"
                         ,"/api/preschool/kid"
-                        ).authenticated()
+                        ).authenticated())
                         //).hasAnyRole("PARENT","TEACHER","ADMIN")
+                        .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.PUT,
                         "/api/album"
                         ,"/api/full"
@@ -99,29 +104,34 @@ public class SecurityConfiguration {
                         ,"/api/teacher"
                         ,"/api/teacher/parent"
                         ,"/api/teacher/parentedit"
-                        //).authenticated()
-                        //).hasAnyRole("TEACHER","ADMIN")
+                        //).authenticated())
+                        //).hasAnyRole("TEACHER","ADMIN"))
+                        //.authorizeHttpRequests(auth -> auth
                         //.requestMatchers(HttpMethod.PUT
                         ,"/api/notice"
-                        ).authenticated()
-                        //).hasAnyRole("PARENT","TEACHER","ADMIN")
+                        ).authenticated())
+                        //).hasAnyRole("PARENT","TEACHER","ADMIN"))
+                        .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.PATCH,
                         "/api/kid/code/{ikid}"
                         ,"/api/teacher"
                         ,"/api/teacher/kid"
-                        //).authenticated()
-                        //).hasAnyRole("TEACHER","ADMIN")
+                        //).authenticated())
+                        //).hasAnyRole("TEACHER","ADMIN"))
+                        //.authorizeHttpRequests(auth -> auth
                         //.requestMatchers(HttpMethod.PATCH
                         ,"/api/parent"
-                        ).authenticated()
-                        //).hasAnyRole("PARENT","TEACHER","ADMIN")
+                        ).authenticated())
+                        //).hasAnyRole("PARENT","TEACHER","ADMIN"))
+                        .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.DELETE,
                         "/api/album"
                         ,"/api/kid"
                         ,"/api/memory"
                         ,"/api/teacher/disconnect"
-                        //).authenticated()
-                        //).hasAnyRole("TEACHER","ADMIN")
+                        //).authenticated())
+                        //).hasAnyRole("TEACHER","ADMIN"))
+                        //.authorizeHttpRequests(auth -> auth
                         //.requestMatchers(HttpMethod.DELETE
                         ,"/api/album/comment"
                         ,"/api/full"
