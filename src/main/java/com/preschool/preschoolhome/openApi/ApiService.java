1bed36da53e77865a60e8b819b1bbf85e0a765bc
GYEONGROK11 <gpol123@naver.com>
2024-02-26 15:29:39 +0900

    2024/02/26 경록

--- src/main/java/com/preschool/preschoolhome/openApi/ApiService.java
+++ src/main/java/com/preschool/preschoolhome/openApi/ApiService.java
@@ -56,7 +56,7 @@ public class ApiService {
                                 .queryParam("Type","json")
                                 .queryParam("Key",openApiProperties.getHospital().getServiceKey())
                                 .queryParam("pIndex",dto.getPage())
-                                .queryParam("pSize",1000)
+                                .queryParam("pSize",300)
                                 .queryParam("SIGUN_NM",dto.getSigunNm())
                                 .build()
 
