9900c55ca93a57a82081af7c0125c5aaa0710675
GYEONGROK11 <gpol123@naver.com>
2024-02-26 15:07:16 +0900

    2024/02/26 경록

--- src/main/java/com/preschool/preschoolhome/memory/MemoryRepository.java
+++ src/main/java/com/preschool/preschoolhome/memory/MemoryRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface MemoryRepository extends JpaRepository<MemoryEntity, Integer>,MemoryQdslRepository {
+public interface MemoryRepository extends JpaRepository<MemoryEntity, Integer>{//,MemoryQdslRepository {
     @EntityGraph(attributePaths = {"teacherEntity"})
     List<MemoryEntity> findAllByTeacherEntityOrderByImemoryDesc(TeacherEntity teacherEntity);
 
