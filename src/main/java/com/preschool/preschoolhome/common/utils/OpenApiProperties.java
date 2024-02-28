1680ae1af10e16ec24ed6fe1c92030ab656ae312
hyun <kimkh8056k@gmail.com>
2024-02-28 10:02:51 +0900

    24/02/28 경현

--- src/main/java/com/preschool/preschoolhome/common/utils/OpenApiProperties.java
+++ src/main/java/com/preschool/preschoolhome/common/utils/OpenApiProperties.java
@@ -9,10 +9,19 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 public class OpenApiProperties {
 
     private final Hospital hospital = new Hospital();
+    private final NightHospital nightHospital = new NightHospital();
 
     @Getter
     @Setter
     public static class Hospital {
+        private String baseUrl;
+        private String dataUrl;
+        private String serviceKey;
+    }
+
+    @Getter
+    @Setter
+    public static class NightHospital {
         private String baseUrl;
         private String dataUrl;
         private String serviceKey;
