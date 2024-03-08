bffadb8bc2cd2df2b01224c630ece96891313c0e
GYEONGROK11 <gpol123@naver.com>
2024-03-08 11:16:16 +0900

    2024/03/07 경록

--- src/main/resources/static/firebase-messaging-sw.js
+++ src/main/resources/static/firebase-messaging-sw.js
@@ -1,40 +1,40 @@
-// Scripts for firebase and firebase messaging
-importScripts(
-  "https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js",
-);
-importScripts(
-  "https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js",
-);
+// // Scripts for firebase and firebase messaging
+// importScripts(
+//   "https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js",
+// );
+// importScripts(
+//   "https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js",
+// );
 
-// Initialize the Firebase app in the service worker by passing the generated config
-const firebaseConfig = {
-  apiKey: "AIzaSyCuIKs9JwV9hTJyHRpthcaeZesm0h-Av1A",
-  authDomain: "preschoolhome-dfd2f.firebaseapp.com",
-  projectId: "preschoolhome-dfd2f",
-  storageBucket: "preschoolhome-dfd2f.appspot.com",
-  messagingSenderId: "1048681571637",
-  appId: "1:1048681571637:web:814c80c8d96efd581b187c",
-  measurementId: "G-0XB87NYZ32",
-};
+// // Initialize the Firebase app in the service worker by passing the generated config
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCuIKs9JwV9hTJyHRpthcaeZesm0h-Av1A",
+//   authDomain: "preschoolhome-dfd2f.firebaseapp.com",
+//   projectId: "preschoolhome-dfd2f",
+//   storageBucket: "preschoolhome-dfd2f.appspot.com",
+//   messagingSenderId: "1048681571637",
+//   appId: "1:1048681571637:web:814c80c8d96efd581b187c",
+//   measurementId: "G-0XB87NYZ32",
+// };
 
-firebase.initializeApp(firebaseConfig);
+// firebase.initializeApp(firebaseConfig);
 
-// Retrieve firebase messaging
-const messaging = firebase.messaging();
+// // Retrieve firebase messaging
+// const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage(function (payload) {
-  console.log("백그라운드일때", payload.notification.body);
-  const body = JSON.parse(payload.notification.body);
-  let title = "백그라운드 메세지";
+// messaging.onBackgroundMessage(function (payload) {
+//   console.log("백그라운드일때", payload.notification.body);
+//   const body = JSON.parse(payload.notification.body);
+//   let title = "백그라운드 메세지";
 
-  const notificationOptions = { body };
+//   const notificationOptions = { body };
 
-  self.registration.showNotification(title, notificationOptions);
-});
-
-// self.addEventListener("notificationclick", function (event) {
-//   console.log("notification click");
-//   const url = "http://localhost:3000";
-//   event.notification.close();
-//   event.waitUntil(clients.openWindow(url));
+//   self.registration.showNotification(title, notificationOptions);
 // });
+
+// // self.addEventListener("notificationclick", function (event) {
+// //   console.log("notification click");
+// //   const url = "http://localhost:3000";
+// //   event.notification.close();
+// //   event.waitUntil(clients.openWindow(url));
+// // });
